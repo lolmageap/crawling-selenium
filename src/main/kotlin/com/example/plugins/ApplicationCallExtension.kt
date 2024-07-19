f0e5270089df854549e67f6750dd9bdbb18bc2b5
lolmageap <ekxk1234@naver.com>
2024-07-19 23:03:45 +0900

    setting to query parameter to class

---
+++ src/main/kotlin/com/example/plugins/ApplicationCallExtension.kt
@@ -0,0 +1,19 @@
+package com.example.plugins
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.ktor.http.*
+import io.ktor.server.application.*
+
+val mapper = jacksonObjectMapper()
+
+inline fun <reified T : Any> ApplicationCall.getQueryParams(): T {
+    return this.request.queryParameters.toClass()
+}
+
+inline fun <reified T : Any> Parameters.toClass(): T {
+    val map = this.entries().associate {
+        it.key to (it.value.getOrNull(0)
+            ?: throw IllegalArgumentException("Missing value for key ${it.key}"))
+    }
+    return mapper.convertValue(map, T::class.java)
+}
