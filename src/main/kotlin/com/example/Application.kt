550e31767467e9700aa3550f53ac1970dba41e4b
lolmageap <ekxk1234@naver.com>
2024-07-19 23:04:19 +0900

    create search and crawling to google api

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -11,4 +11,5 @@ fun Application.module() {
     configureSerialization()
     configureDatabases()
     configureRouting()
+    configureCrawling()
 }
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,13 +1,25 @@
 package com.example.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
+// TODO: dependency injection 설정 해야함.
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        post("/google") {
+            val request = call.receive<SearchRequest>()
+            CrawlingExecutor().execute(request.keyword)
+        }
+
+        post("/google2") {
+            val request = call.receive<SearchRequest>()
+            CrawlingExecutor().executeAndClose(request.keyword)
         }
     }
 }
